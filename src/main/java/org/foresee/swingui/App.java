467b3fe82dbe75b16f4a24b0d16d80e07f951780
Foresee2016 <1123520670@qq.com>
2018-05-28 21:02:35 +0800

    Change App's LookAndFeel to Nimbus.

--- src/main/java/org/foresee/swingui/App.java
+++ src/main/java/org/foresee/swingui/App.java
@@ -1,5 +1,9 @@
 package org.foresee.swingui;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
 import org.foresee.swingui.dialog.MainFrame;
 
 /**
@@ -10,6 +14,11 @@ public class App
 {
     public static void main( String[] args )
     {
+    	try {
+			UIManager.setLookAndFeel(new NimbusLookAndFeel()); //Nimbus水晶风格
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
         new MainFrame();
     }
 }
