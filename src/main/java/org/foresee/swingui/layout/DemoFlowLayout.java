88ee4df27d749442f9327ab1d775281e2bb951d0
Foresee2016 <1123520670@qq.com>
2018-05-29 13:52:44 +0800

    Add DemoFlowLayout to see FlowLayout's effect.

---
+++ src/main/java/org/foresee/swingui/layout/DemoFlowLayout.java
@@ -0,0 +1,25 @@
+package org.foresee.swingui.layout;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class DemoFlowLayout {
+public static void main(String[] args) {
+	JFrame frame=new JFrame("FlowLayout");
+	frame.setSize(200, 250);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setLocationRelativeTo(null);
+	
+	JPanel panel=new JPanel(new FlowLayout());
+	panel.add(new JButton("Button 1"));
+	panel.add(new JButton("Button 2"));
+	panel.add(new JButton("Button 3"));
+	panel.add(new JButton("Button 4"));
+	panel.add(new JButton("Button 5"));
+	frame.setContentPane(panel);
+	frame.setVisible(true);
+}
+}
