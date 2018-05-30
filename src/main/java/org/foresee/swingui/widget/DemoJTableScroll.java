38d45481078532a248d57d316ae6dfd03105e083
Foresee2016 <1123520670@qq.com>
2018-05-30 11:08:47 +0800

    Modify DemoJTableScroll to use JPanel as container of JScrollPane.

--- src/main/java/org/foresee/swingui/widget/DemoJTableScroll.java
+++ src/main/java/org/foresee/swingui/widget/DemoJTableScroll.java
@@ -1,6 +1,9 @@
 package org.foresee.swingui.widget;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
@@ -9,6 +12,7 @@ public class DemoJTableScroll {
 	public static void main(String[] args) {
 		JFrame frame=new JFrame("JTable");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel=new JPanel();
 		// 表头（列名称）
 		Object[] colNames={"姓名","语文","数学","英语","总分"};
 		// 内容数据
@@ -29,10 +33,13 @@ public class DemoJTableScroll {
 		/*
 		 * NOTE：如果JTable添加到JScrollPane容器中， 标题会固定，内容滚动
 		 */
+		table.setRowHeight(30);
+		table.setPreferredScrollableViewportSize(new Dimension(400, 200));
 		JScrollPane scrollPane=new JScrollPane(table, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		frame.setContentPane(scrollPane);
-		frame.setSize(400, 200);
+		panel.add(scrollPane);
+		frame.setContentPane(panel);
+		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
